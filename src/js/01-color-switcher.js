bb746d61ddf33e6f9153b6325cfd691ac076075e
AntonPavlov8 <barsikstd@gmail.com>
2023-03-18 20:39:19 -0500

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBT = document.querySelector('button[data-start]');
+const stopBT = document.querySelector('button[data-stop]');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+let colorSwitcher;
+
+startBT.addEventListener('click', () => {
+  colorSwitcher = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 500);
+
+  startBT.setAttribute('disabled', '');
+  stopBT.removeAttribute('disabled');
+});
+stopBT.addEventListener('click', () => {
+  clearInterval(colorSwitcher);
+
+  stopBT.setAttribute('disabled', '');
+  startBT.removeAttribute('disabled');
+});
