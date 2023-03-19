145cf6b0aeccdf731fb4d67d4e12271121388d30
AntonPavlov8 <112436831+AntonPavlov8@users.noreply.github.com>
2023-03-19 01:39:43 +0000

    Deploying to gh-pages from @ AntonPavlov8/goit-js-hw-09@bb746d61ddf33e6f9153b6325cfd691ac076075e 🚀

---
+++ 01-color-switcher.efc14d3e.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let r;t.addEventListener("click",(()=>{r=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),500),t.setAttribute("disabled",""),e.removeAttribute("disabled")})),e.addEventListener("click",(()=>{clearInterval(r),e.setAttribute("disabled",""),t.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.efc14d3e.js.map
